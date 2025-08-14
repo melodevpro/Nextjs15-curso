7b3e90adde1c6fb5315e3f88f100cb2073f0a30e
Matthew Duarte <hello.codehipster@outlook.com>
2025-08-14 14:42:42 -0500

    capturar segmentos

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,7 @@
 export default async function ProductReview({
     params,
 }: {
-    params: Promise< {productId: string; reviewId:string} >;
+    params: Promise< {productId: string; reviewId: string} >;
 }) {
     const { productId, reviewId} = await params;
     return (
