1bacfb719fb77d15717877ac76d89e5d7bc9d03b
Matthew Duarte <hello.codehipster@outlook.com>
2025-08-14 14:23:30 -0500

    rutas dinamicas anidadas

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList(){
+    return (
+        <>
+        <h1>Lista de productos</h1>
+        <h2>Producto 1</h2>
+        <h2>Producto 2</h2>
+        <h2>Producto 3</h2>
+        </>
+    );
+}
